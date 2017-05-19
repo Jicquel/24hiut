159579c41b8a289e26aa7e723b1b808119a688e5
jicquelv <jicquelv@localhost.localdomain>
2017-05-19 16:36:19 +0200

    Moule.java

---
+++ Moule.java
@@ -0,0 +1,26 @@
+
+public class Moule
+{
+        private int valeur;
+        private int x;
+        private int y;
+
+        public Moule(int x0, int y0, int val0)
+        {
+          this.x=x0;
+          this.y=y0;
+          this.valeur=val0;
+        }
+        public int getX()
+        {
+          return this.x;
+        }
+        public int getY()
+        {
+          return this.y;
+        }
+        public int getVal()
+        {
+          return this.valeur;
+        }
+}
